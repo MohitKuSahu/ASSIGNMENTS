92a5e9a996276b7c1f5cb9d67e640289e238cf47
MohitKuSahu <114129187+MohitKuSahu@users.noreply.github.com>
2024-02-11 14:33:06 +0530

    Extension Function
    It changes a generic list to datatable and vice versa.

---
+++ Extension&Genericclass/Extension&Genericclass/Program.cs
@@ -0,0 +1,37 @@
+﻿using AdoNetConsoleApplication;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Genericclass
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
+                con.Open();
+                ListtoDatatable.List();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                DatatabletoList.Data();
+            } catch (Exception e)
+            {
+                Console.WriteLine("OOPS! Error ");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+       
+}
+}
