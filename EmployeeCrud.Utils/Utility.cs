7dbfab7c2855a05e13242d14d004e6db0dd786cb
MohitKuSahu <114129187+MohitKuSahu@users.noreply.github.com>
2024-02-12 18:03:32 +0530

    Utils Entity Data Model

---
+++ EmployeeCrud.Utils/Utility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeCrud.Utils
+{
+    public class Utility
+    {
+
+        public enum Op
+        {
+            ViewEmployee = 1,
+            InsertEmployee,
+            UpdateEmployee,
+            DeleteEmployee,
+            SearchEmployee,
+            ViewBranch,
+            InsertBranch,
+            UpdateBranch,
+            DeleteBranch,
+            ViewDepartment,
+            InsertDepartment,
+            UpdateDepartment,
+            DeleteDepartment
+        }
+    }
+}
